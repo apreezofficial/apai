04a40b8cf9f47fabec697e9d43255b833f062c7d
apreezofficial <apreezofficial@gmail.com>
2025-03-12 09:42:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const createChatLi = (message, className) => {
   // Create a chat <li> element with passed message and className
   const chatLi = document.createElement("li");
   chatLi.classList.add("chat", `${className}`);
-  let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+  let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined"><img src="Screenshot_2025-03-12-09-37-35-88_e5d3893ac03954c6bb675ef2555b879b (1).jpg"></span><p></p>`;
   chatLi.innerHTML = chatContent;
   chatLi.querySelector("p").textContent = message;
   return chatLi; // return chat <li> element
